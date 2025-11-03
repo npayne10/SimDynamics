e8c5aeafb921797b91f7e0cdf17fe446bc62268f
Neill Payne <npayne@g-host.co.za>
2025-11-03 19:39:59 +0200

    Add files via upload

---
+++ SDRacingHub/iRacingPlugin/AiRacingPlugin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using vAzhureRacingAPI;
+
+namespace iRacingPlugin
+{
+    public class AiRacingPlugin : ICustomPlugin, IDisposable 
+    {
+        readonly IRacingGame game = new IRacingGame();
+
+        public string Name => "iRacing";
+
+        public string Description => "Плагин iRacing для vAzhure Racing Hub\nАвтор: Журавлев Андрей";
+
+        public ulong Version => 1L;
+
+        public bool CanClose(IVAzhureRacingApp app)
+        {
+            return true;
+        }
+
+        public void Dispose()
+        {
+            Quit(null);
+        }
+
+        public bool Initialize(IVAzhureRacingApp app)
+        {
+            app.RegisterGame(game);
+            try
+            {
+                game.Initialize(app);
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public void Quit(IVAzhureRacingApp app)
+        {
+            game.Quit(app);
+        }
+    }
+}
